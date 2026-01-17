dbac17cb72dd83f4dbe65d8e7d9bd077c116cbe5
Ahmed Abdelnaby <120122678+Makarona10@users.noreply.github.com>
2026-01-17 20:47:47 +0200

    Create S3T2.jsx with a simple React component

---
+++ S3T2.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState } from 'react'
+
+
+function App() {
+  return (
+    <>
+      <div className='p-12 m-16 text-xl'>
+        Tailwind CSS is a great framework!
+      </div>
+    </>
+  )
+}
+
+
+export default App
